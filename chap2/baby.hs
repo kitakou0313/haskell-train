9ccc518fb95ceb531a128613269dfe267838ed31
kitakou0313 <kitakou0313@gmail.com>
2023-02-22 03:12:10 +0000

    Define func to use tuple and list

--- chap2/baby.hs
+++ chap2/baby.hs
@@ -13,3 +13,15 @@ length' xs = sum [1| _ <- xs]
 -- 内包表記
 
 removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+-- tuple in haskell
+-- 格納するvalueのtype, 個数毎に型をもつ
+-- すべてのtupleに使えるような関数は定義できない
+-- listには同じ型のvalueしか格納できないので[(1,"Hoge"), (2,3)]みたいなことはできない
+
+zip' = zip [1..] [2..10]
+-- 無限長listを扱えるのでこういうこともできる
+
+triangles = [(a,b,c) | a <- [1..10], b <- [1..10], c <- [1..10], c^2 == a^2 + b^2, a+b+c == 24]
+-- 基本的なfunctional programingの一例
+-- solutionの一覧を列挙, filter用の変換を定義, 最終的な答えを返す
