271d6eed7429ea9d4785c9f8c74c8826a853b8d9
kitakou0313 <kitakou0313@gmail.com>
2023-02-22 02:52:31 +0000

    Add sample of list comprehensions

--- chap2/baby.hs
+++ chap2/baby.hs
@@ -6,3 +6,10 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x*2
 -- if statementもexpression（valueを返すもの）の一種->なんらかの値を返すものと見なせるので以下のように書ける
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs , odd x]
+
+length' xs = sum [1| _ <- xs]
+-- 内包表記
+
+removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
