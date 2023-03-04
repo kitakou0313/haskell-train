64ad56efac73188ae6a13f704f12c79abc726147
kitakou0313 <kitakou0313@gmail.com>
2023-03-04 06:46:50 +0000

    Add sample of filter and map func

--- chap6/higher_order_function.hs
+++ chap6/higher_order_function.hs
@@ -70,3 +70,29 @@ quicksort' (x:xs) =
     let smallerSorted = quicksort' (filter (<=x) xs)
         biggerSorted = quicksort' (filter' (>x) xs)
     in smallerSorted ++ [x] ++ biggerSorted
+
+
+largestDivisible ::(Integral a) => a
+largestDivisible = head  (filter p [100000, 99999..])
+        where p x = (x `mod` 3829) == 0
+
+sumAllOddSquares :: (Integral a) => a
+sumAllOddSquares = sum (
+    takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+sumAllOddSquares' ::(Integral a) => a
+sumAllOddSquares' = sum(
+    takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
+
+chain :: (Integral a) => a -> [a]
+chain 1 = []
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain(n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length(filter isLongChain [chain n | n <- [1..100]])
+    where isLongChain xs = length xs > 15
+
+-- 引数を複数とる関数をmapに渡すことでcurried functionのlistを作れる
+listOfFunx = map (*) [0..]
