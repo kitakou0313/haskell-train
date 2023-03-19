e2bcb3a4e6bdfb337369b66b383aedb495dc2953
kitakou0313 <kitakou0313@gmail.com>
2023-03-19 02:40:44 +0000

    Add memo about modules impotings

--- chap8/modules.hs
+++ chap8/modules.hs
@@ -1,5 +1,15 @@
 -- import時は文頭でimport構文を用いる
 import Data.List
 
+-- 関数を指定して読み込み
+import Data.List (nub, sort)
+-- 指定した関数を除外して読み込み
+-- いくつかのmoduleで名前が重複してるときに便利
+import Data.List hiding (nub)
+
+-- 名前空間を参照する形でimportもできる
+import qualified Data.Map
+import qualified Data.Map as M
+
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
