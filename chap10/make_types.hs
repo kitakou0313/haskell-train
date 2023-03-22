e916b7e314a9d5e8135442295a66c1d4ea29e5df
kitakou0313 <kitakou0313@gmail.com>
2023-03-22 00:17:47 +0000

    Add memo to pattern match with vc

--- chap10/make_types.hs
+++ chap10/make_types.hs
@@ -39,3 +39,7 @@ data Shape2 = Circle2 Point Float | Rectangle2 Point Point deriving (Show)
 surface2 :: Shape2 -> Float
 surface2 (Circle2 _ r) = pi * r ^ 2
 surface2 (Rectangle2 (Point x1 y1) (Point x2 y2)) = (abs $ x1 - x2) * (abs $ y1 - y2)
+
+nudge :: Shape2 -> Float -> Float -> Shape2
+nudge (Circle2 (Point x y) r) a b = Circle2 (Point (x+a) (y+b)) r
+nudge (Rectangle2 (Point x1 y1) (Point x2 y2)) a b = Rectangle2 (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2+b))
