c7dfc48dcc06d5e0ab76a7004e8ebe0a3c4a6e06
kitakou0313 <kitakou0313@gmail.com>
2023-04-03 10:06:52 +0000

    Add sample of intercalate

--- chap8/list.hs
+++ chap8/list.hs
@@ -2,3 +2,6 @@ import qualified Data.List as List
  
 --  第二引数で与えられたリストの要素の間に第一引数の要素を入れる
 res = List.intersperse '.' "MONKEY"
+
+-- listの間にリストを入れて結合する
+res2 = List.intercalate [0,0] [[1,2,3],[4,5,6]]
