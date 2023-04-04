dbd717bfcb68cd09c662d7638d34e42199cce6ce
kitakou0313 <kitakou0313@gmail.com>
2023-04-04 00:17:28 +0000

    Add sample of transpose and concat

--- chap8/list.hs
+++ chap8/list.hs
@@ -5,3 +5,11 @@ res = List.intersperse '.' "MONKEY"
 
 -- listの間にリストを入れて結合する
 res2 = List.intercalate [0,0] [[1,2,3],[4,5,6]]
+
+-- 2D行列を転置する
+res3 = List.transpose [[1,2,3],[4,5,6],[7,8,9]]
+res4 = map sum $ List.transpose [[0,3,5,9],[10,0,0,9],[8,5,1,-1]]
+
+-- リスト内要素の結合
+res5 = List.concat ["foo", "bar", "cat"]
+res6 = List.concat [[3,4,5],[2,3,4]]
