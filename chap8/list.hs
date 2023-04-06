9264fb29d421c83b0934fb52f839d260ba6d7d8b
kitakou0313 <kitakou0313@gmail.com>
2023-04-06 00:04:39 +0000

    Add samples of Data.List func

--- chap8/list.hs
+++ chap8/list.hs
@@ -13,3 +13,18 @@ res4 = map sum $ List.transpose [[0,3,5,9],[10,0,0,9],[8,5,1,-1]]
 -- リスト内要素の結合
 res5 = List.concat ["foo", "bar", "cat"]
 res6 = List.concat [[3,4,5],[2,3,4]]
+
+-- mapして得られたlist in listを結合
+res7 = List.concatMap (replicate 4) [1..3]
+
+-- list内のbool値すべてに対して演算する
+res8 = and $ map (>4) [5,6,7,8]
+res9 = or $ map (>4) [1,2,3,5,6,7]
+
+-- 実際はanyとallが代わりに使われる
+res10 = any (==4) [1,2,3,4]
+res11 = all (==4) [1,2,3,4]
+
+-- 初期値と関数を取り，無限長リストを生成
+res12 = take 10 $ List.iterate (*2) 1
+-- [1,2,4,8]
